2641165c88c2defe2c014d2bfede77dc9e011b56
Ratteelouis <149548112+Ratteelouis@users.noreply.github.com>
2023-12-04 14:32:48 +0700

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,13 @@
+let coming1 = document.getElementById('coming1')
+let coming2 = document.getElementById('coming2')
+let coming3 = document.getElementById('coming3')
+
+coming1.onclick = () => {
+    alert('Coming soon')
+}
+coming2.onclick = () => {
+    alert('Coming soon')
+}
+coming3.onclick = () => {
+    alert('Coming soon')
+}
